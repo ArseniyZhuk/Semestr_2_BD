7b68e2b86d9d37899d634a6a2009ef2ea86df1f2
ArseniyZhuk <ars4zhuk@gmail.com>
2023-12-28 10:56:29 +0400

    Lr_7

---
+++ Lr_7/Task_7_4.sql
@@ -0,0 +1,50 @@
+/* напишите функцию, которая будет рассчитывать увеличение/уменьшение стоимость аренды объекта на последующие месяцы  для изменения (увеличения или уменьшения) 
+срока окупаемость на заданную долю (в процентах) на основании расчета окупаемости за уже оплаченные периоды. Сохраните расчет в виде csv или sql файла (например, используя временные таблицы) */
+
+USE cd;
+
+DELIMITER &&
+
+CREATE FUNCTION paybackPeriod(facid INT, part FLOAT, 
+duration TIMESTAMP)
+    RETURNS DECIMAL(10, 2)
+    READS SQL DATA
+    NOT DETERMINISTIC
+BEGIN
+    DECLARE revenue DECIMAL(10, 2);
+    DECLARE service DECIMAL(10, 2);
+    DECLARE profit DECIMAL(10, 2);
+    DECLARE expenses DECIMAL(10, 2);
+    SELECT SUM(p.payment) INTO revenue
+    FROM payments AS p
+    JOIN bookings AS b ON b.bookid = p.bookid
+    JOIN facilities AS f ON b.facid = f.facid
+    WHERE facid = b.facid AND b.starttime <= duration
+    GROUP BY b.facid;
+    IF revenue IS NULL THEN
+        RETURN 1;
+    END IF;
+    SELECT f.monthlyservice * (MONTH(duration) - 
+    MONTH(MIN(b.starttime)) + 1) INTO service
+    FROM facilities AS f
+    JOIN bookings AS b ON b.facid = f.facid
+    WHERE facid = b.facid
+    GROUP BY b.facid;
+    SELECT f.initialoutlay INTO expenses
+    FROM facilities AS f
+    WHERE facid = f.facid
+    LIMIT 1;
+    SET profit = (revenue - service);
+    IF profit <= 0 OR expenses <= profit THEN
+        RETURN 1;
+    END IF;
+    RETURN ((1 / part) * (expenses - profit) / expenses * (1 - 
+    service / revenue) + service / revenue);
+END &&
+
+DELIMITER ;
+
+SELECT paybackPeriod(2, 2, '2012-07-31-23:59:59')
+INTO OUTFILE 'rental_change_result.csv' 
+    FIELDS ENCLOSED BY '"' TERMINATED BY ';' ESCAPED BY '"' 
+    LINES TERMINATED BY '\r\n';
---
+++ Lr_7/Task_7_5.sql
@@ -0,0 +1,21 @@
+/* произведите изменения в стоимости аренды объектов согласно расчету задания Task-7-4, написав update-скрипт. 
+напишите скрипт отмечающий оплату всех аренд за , используя функцию Task-7-3 . 
+сравните, совпадают ли расчетные данные с теми, что были Вами получены в задании Task-7-4. */  
+
+USE cd;
+
+START TRANSACTION;
+    -- Task_7_3.sql
+    CALL recoupment(1, MONTH('2012-07-03'), YEAR('2012-07-03'));
+    UPDATE facilities
+    SET
+        guestcost = guestcost * (SELECT paybackPeriod(facid, 
+        2, '2012-07-31-23:59:59')),
+        membercost = membercost * (SELECT paybackPeriod(facid, 
+        2, '2012-07-31-23:59:59'));
+    UPDATE bookings
+    SET payed = 1
+    WHERE DATE(starttime) < '2012-09-01' AND DATE(starttime) >= '2012-08-01';
+    -- Task_7_3.sql + 1 месяц
+    CALL recoupment(1, MONTH('2012-08-03'), YEAR('2012-08-03'));
+ROLLBACK; -- или COMMIT тогда сохранятся изменения, сделанные в рамках транзакции
